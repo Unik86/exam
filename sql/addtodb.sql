b4ae458d750bbcdf3b97eda3ff4070b6b728e0fc
Unik86 <unik3d@ukr.net>
2014-12-09 08:37:20 +0200

    add sql files

---
+++ sql/addtodb.sql
@@ -0,0 +1,48 @@
+﻿
+INSERT INTO Question (textQuestion) VALUES ('Какого цвета солнце?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (1, 'Синее', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (1, 'Жёлтое', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (1, 'Зеленое', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Сколько месяцев в году?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (2, '3', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (2, '7', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (2, '12', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (2, '8', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (2, '10', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Сколько будет 2+2?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (3, '4', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (3, '5', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (3, '3', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Как зовут поэта Шевченко?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (4, 'Саша', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (4, 'Петя', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (4, 'Тарас', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (4, 'Джон', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Зимой и летом одним цветом?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (5, 'Ёлка', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (5, 'Груша', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (5, 'Орех', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (5, 'Тополь', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (5, 'Береза', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Где живет Санта?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (6, 'Кипр', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (6, 'Умань', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (6, 'Лапландия', true);
+
+INSERT INTO Question (textQuestion) VALUES ('Столица Украины?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (7, 'Киев', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (7, 'Харьков', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (7, 'Одесса', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (7, 'Бонабия', false);
+
+INSERT INTO Question (textQuestion) VALUES ('Какого цвета снег?');
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (8, 'Жёлтый', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (8, 'Синий', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (8, 'Белый', true);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (8, 'Красный', false);
+INSERT INTO Answer (idQuestion_parent, textAnswer, isRight) VALUES (8, 'Розовый', false);
---
+++ sql/database.sql
@@ -0,0 +1,23 @@
+﻿DROP DATABASE IF EXISTS Exam;
+CREATE DATABASE Exam DEFAULT CHARACTER SET 'utf8';
+USE Exam;
+--  Вопросы
+CREATE TABLE Question (
+	idQuestion		INT UNSIGNED 	NOT NULL  AUTO_INCREMENT COMMENT "Код вопроса",
+	textQuestion 	CHAR(100) 		COMMENT "Текст вопроса",
+	
+	CONSTRAINT xidQuestion PRIMARY KEY (idQuestion)
+) COMMENT "Таблица вопросов";
+
+-- Ответы
+CREATE TABLE Answer (
+	idAnswer				INT UNSIGNED 	NOT NULL  AUTO_INCREMENT COMMENT "Код ответа",
+	idQuestion_parent 		INT UNSIGNED 	COMMENT "Код вопроса родителя",
+	textAnswer		 		CHAR(100) 		COMMENT "Текст ответа",
+	isRight 				BOOLEAN 		COMMENT "Правильный ли ответ",
+	
+	CONSTRAINT xidAnswer PRIMARY KEY (idAnswer),
+	INDEX xidQuestion_parent (idQuestion_parent),
+	CONSTRAINT  fidQuestion_parent FOREIGN KEY (idQuestion_parent)
+		REFERENCES Question(idQuestion) ON DELETE CASCADE
+) COMMENT "Таблица ответов";
